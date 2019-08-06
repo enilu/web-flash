eea07db534c6fc82ac521f93d3f926fa2cacf55d
enilu <zhangtao54@gmail.com>
2019-08-06 13:52:18 +0800

    change 完善文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,6 +22,7 @@ module.exports = {
                     {text: '定时任务',link:'/action/task'},
                     {text: '缓存',link:'/action/cache'},
                     {text: '审计功能',link:'/action/jpaauditing'},
+                    {text: 'CMS',link:'/feature/cms'}
                 ]
             },
             {text: '周边',
@@ -69,7 +70,9 @@ module.exports = {
                     '/feature/monitor',
                     '/feature/sysConfig',
                     '/feature/fileMgr',
-                    '/feature/messageCenter'
+                    '/feature/exportExcel',
+                    '/feature/messageCenter',
+                    '/feature/cms.md'
                 ]
             },{
                 title: '进阶',
@@ -80,6 +83,7 @@ module.exports = {
                     '/action/task',
                     '/action/jpaauditing.md',
                     '/action/validator'
+
 
                 ]
             },{
