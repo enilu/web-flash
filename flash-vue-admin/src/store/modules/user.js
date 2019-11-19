96a2ec65ff83c3f50dfd44845f9f29fbc2d2fcda
enilu <zhangtao54@gmail.com>
2019-11-19 17:23:40 +0800

    Fix bug #39

--- flash-vue-admin/src/store/modules/user.js
+++ flash-vue-admin/src/store/modules/user.js
@@ -72,6 +72,7 @@ const actions = {
     return new Promise((resolve, reject) => {
       logout(state.token).then(() => {
         commit('SET_TOKEN', '')
+        commit('SET_NAME', '')
         removeToken()
         resetRouter()
         resolve()
