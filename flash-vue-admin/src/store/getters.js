0ba6112f6799eabe56b633455c6b569cba1d681e
enilu <zhangtao54@gmail.com>
2020-01-16 10:52:11 +0800

    Fix 权限相关问题

--- flash-vue-admin/src/store/getters.js
+++ flash-vue-admin/src/store/getters.js
@@ -4,6 +4,7 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   avatar: state => state.user.avatar,
-  name: state => state.user.name
+  name: state => state.user.name,
+  roles: state => state.user.roles
 }
 export default getters
