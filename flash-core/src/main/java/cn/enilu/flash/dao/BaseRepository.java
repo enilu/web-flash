2d8af272d9235be1277d84d2d61e9467ac515ef3
enilu <zhangtao54@gmail.com>
2020-03-09 11:00:09 +0800

    Issue #I1B3M2 BaseRepository封装根据原生sql查询数据返回Map或List<Map>

--- flash-core/src/main/java/cn/enilu/flash/dao/BaseRepository.java
+++ flash-core/src/main/java/cn/enilu/flash/dao/BaseRepository.java
@@ -1,5 +1,6 @@
 package cn.enilu.flash.dao;
 
+import cn.enilu.flash.bean.vo.query.SearchFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
@@ -7,6 +8,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 封装基础的dao接口
@@ -18,12 +20,9 @@ import java.util.List;
 public interface BaseRepository<T, ID extends Serializable> extends JpaRepository<T, ID>
         , PagingAndSortingRepository<T, ID>
         , JpaSpecificationExecutor<T> {
-    /**
-     * 根据原生sql语句查询数据列表
-     * @param sql
-     * @return
-     */
-    List<Object[]> queryBySql(String sql);
+    List<Map> queryBySql(String sql);
+    Map queryBysql(String sql,List<SearchFilter> filters);
+    List<Map> queryBySql(String sql, List<SearchFilter> filters);
 
     /**
      * 根据原生sql查询对象列表
--- flash-core/src/main/java/cn/enilu/flash/dao/BaseRepositoryImpl.java
+++ flash-core/src/main/java/cn/enilu/flash/dao/BaseRepositoryImpl.java
@@ -1,11 +1,17 @@
 package cn.enilu.flash.dao;
 
+import cn.enilu.flash.bean.vo.query.SearchFilter;
+import cn.enilu.flash.utils.Lists;
+import org.hibernate.query.internal.NativeQueryImpl;
+import org.hibernate.transform.Transformers;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 基础dao实现类
@@ -27,9 +33,33 @@ public class BaseRepositoryImpl<T, ID extends Serializable>
         this.klass = (Class<T>) entityInformation.getJavaType();
     }
 
+
     @Override
-    public List<Object[]> queryBySql(String sql) {
-        return entityManager.createNativeQuery(sql).getResultList();
+    public List<Map> queryBySql(String sql) {
+        return queryBySql(sql, Lists.newArrayList());
+    }
+
+    @Override
+    public Map queryBysql(String sql, List<SearchFilter> filters) {
+        List<Map> list = queryBySql(sql,filters);
+        if(list!=null&&!list.isEmpty()){
+            list.get(0);
+        }
+        return null;
+    }
+
+    @Override
+    public List<Map> queryBySql(String sql, List<SearchFilter> filters) {
+        Query query = entityManager.createNativeQuery(sql);
+        if(filters!=null&&!filters.isEmpty()){
+            for(SearchFilter filter:filters){
+                query.setParameter(filter.fieldName, filter.value);
+            }
+        }
+        query.unwrap(NativeQueryImpl.class)
+                .setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+        List list = query.getResultList();
+        return list;
     }
 
     @Override
