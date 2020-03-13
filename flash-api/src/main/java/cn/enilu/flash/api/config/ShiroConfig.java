1fd1cb49c627b7676e498d20cb98189af8915ca0
enilu <zhangtao54@gmail.com>
2020-03-13 14:54:26 +0800

    Fix bug 后极端权限配置问题

--- flash-api/src/main/java/cn/enilu/flash/api/config/ShiroConfig.java
+++ flash-api/src/main/java/cn/enilu/flash/api/config/ShiroConfig.java
@@ -84,7 +84,7 @@ public class ShiroConfig {
         filterRuleMap.put("/account/login","anon");
         filterRuleMap.put("/logout", "logout");
         //H5前端不拦截
-        filterRuleMap.put("/offcialsite","anon");
+        filterRuleMap.put("/offcialsite/**","anon");
         // 访问401和404页面不通过我们的Filter
         filterRuleMap.put("/401", "anon");
         filterRuleMap.put("/**", "jwt");
