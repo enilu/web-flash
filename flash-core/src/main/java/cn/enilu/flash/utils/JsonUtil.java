d75bf8dca4a45c061a1ba66ec27dad4333610800
enilu <zhangtao54@gmail.com>
2020-06-09 10:49:38 +0800

    Issue 封装JsonUtil,统一供外部使用

--- flash-core/src/main/java/cn/enilu/flash/utils/JsonUtil.java
+++ flash-core/src/main/java/cn/enilu/flash/utils/JsonUtil.java
@@ -1,18 +1,20 @@
 package cn.enilu.flash.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import cn.enilu.flash.bean.entity.system.User;
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 /**
+ * 封装json工具类<br>
+ *  通过该类减少项目中对特定的json库依赖，方便统一切换json库，目前使用nutz的Json工具类
  * @author ：enilu
  * @date ：Created in 2020/5/31 21:55
  */
 public class JsonUtil {
-    public static ObjectMapper objectMapper;
     public static  String toJsonForHuman(Object obj ){
         return Json.toJson(obj);
     }
@@ -20,7 +22,7 @@ public class JsonUtil {
       return Json.toJson(obj, JsonFormat.compact());
     }
     public static  Object fromJson(String jsonStr){
-        return StringUtil.isNotEmpty(jsonStr)?fromJson(Map.class,jsonStr):null;
+        return StringUtil.isNotEmpty(jsonStr)?Json.fromJson(jsonStr):null;
     }
 
     public static <T> T fromJson (Class<T> valueType,String jsonStr) {
@@ -30,5 +32,41 @@ public class JsonUtil {
     return Json.fromJsonAsList(valueType,jsonStr);
     }
 
+    public static void main(String[] args) {
+        User user = new User();
+        user.setAccount("admin");
+        user.setCreateTime(new Date());
+
+        String json = toJson(user);
+
+        System.out.println("压缩后的json字符串：=======》");
+        System.out.println(json);
+
+        System.out.println("方便调试查看的json字符串：==》");
+        System.out.println(toJsonForHuman(user));
+
+        User user2 = fromJson(User.class,json);
+
+        System.out.println("json字符串转对象：==========》");
+        System.out.println(user2.getAccount()+user2.getCreateTime());
+
+        Object obj = fromJson(json);
+
+        System.out.println("json字符串转换为map========》");
+        System.out.println(obj instanceof Map);
+
+
+
+        List<User> users = Lists.newArrayList(user);
+        String jsons = toJson(users);
+        List<User> users2 = fromJsonAsList(User.class,jsons );
+        System.out.println("转换为集合后的集合长度：=======》");
+        System.out.println(users2.size());
+
+        Object obj2 = fromJson(jsons);
+        System.out.println("json字符串转换为List========》");
+        System.out.println(obj2 instanceof List);
+
+    }
 
 }
