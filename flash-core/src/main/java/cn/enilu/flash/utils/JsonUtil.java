dcaf20063700e5e58b156fbf4c7d9e7de98f4348
enilu <zhangtao54@gmail.com>
2020-06-09 11:07:21 +0800

    Issue 封装JsonUtil使用jackson代替nutz的json工具类

--- flash-core/src/main/java/cn/enilu/flash/utils/JsonUtil.java
+++ flash-core/src/main/java/cn/enilu/flash/utils/JsonUtil.java
@@ -1,35 +1,68 @@
 package cn.enilu.flash.utils;
 
 import cn.enilu.flash.bean.entity.system.User;
-import org.nutz.json.Json;
-import org.nutz.json.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 封装json工具类<br>
- *  通过该类减少项目中对特定的json库依赖，方便统一切换json库，目前使用nutz的Json工具类
+ *  通过该类减少项目中对特定的json库依赖，方便统一切换json库，目前使用jackson
  * @author ：enilu
  * @date ：Created in 2020/5/31 21:55
  */
 public class JsonUtil {
     public static  String toJsonForHuman(Object obj ){
-        return Json.toJson(obj);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        } catch (Exception e) {
+        }
+        return "";
     }
     public static  String toJson(Object obj){
-      return Json.toJson(obj, JsonFormat.compact());
+        StringWriter sw = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(sw, obj);
+        } catch (Exception e) {
+        }
+        return sw.toString();
     }
-    public static  Object fromJson(String jsonStr){
-        return StringUtil.isNotEmpty(jsonStr)?Json.fromJson(jsonStr):null;
+    public static  String toJsonNotNull(Object obj){
+        StringWriter sw = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            mapper.writeValue(sw, obj);
+        } catch (Exception e) {
+        }
+        return sw.toString();
     }
 
-    public static <T> T fromJson (Class<T> valueType,String jsonStr) {
-     return Json.fromJson(valueType,jsonStr);
+    public static <T> T fromJson (Class<T> klass,String jsonStr) {
+        ObjectMapper mapper = new ObjectMapper();
+        T obj = null;
+        try {
+            obj = mapper.readValue(jsonStr, klass);
+        } catch (Exception e) {
+        }
+        return obj;
     }
-    public static <T> List<T> fromJsonAsList(Class<T> valueType,String jsonStr){
-    return Json.fromJsonAsList(valueType,jsonStr);
+    public static <T> List<T> fromJsonAsList(Class<T> klass,String jsonStr){
+        ObjectMapper mapper = new ObjectMapper();
+        List<T> objList = null;
+        try {
+            JavaType t = mapper.getTypeFactory().constructParametricType(
+                    List.class, klass);
+            objList = mapper.readValue(jsonStr, t);
+        } catch (Exception e) {
+        }
+        return objList;
     }
 
     public static void main(String[] args) {
@@ -39,33 +72,25 @@ public class JsonUtil {
 
         String json = toJson(user);
 
-        System.out.println("压缩后的json字符串：=======》");
+        System.out.println("压缩后的json字符串：========》");
         System.out.println(json);
 
-        System.out.println("方便调试查看的json字符串：==》");
+        System.out.println("方便调试查看的json字符串：===》");
         System.out.println(toJsonForHuman(user));
+
+        System.out.println("不输出值为null的json字符串：==》");
+        System.out.println(toJsonNotNull(user));
 
         User user2 = fromJson(User.class,json);
 
         System.out.println("json字符串转对象：==========》");
         System.out.println(user2.getAccount()+user2.getCreateTime());
 
-        Object obj = fromJson(json);
-
-        System.out.println("json字符串转换为map========》");
-        System.out.println(obj instanceof Map);
-
-
-
         List<User> users = Lists.newArrayList(user);
         String jsons = toJson(users);
         List<User> users2 = fromJsonAsList(User.class,jsons );
         System.out.println("转换为集合后的集合长度：=======》");
         System.out.println(users2.size());
-
-        Object obj2 = fromJson(jsons);
-        System.out.println("json字符串转换为List========》");
-        System.out.println(obj2 instanceof List);
 
     }
 
