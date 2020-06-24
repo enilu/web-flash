f06ab13f91be8ba122b1fae8fd714f0fd1fc16b8
zhangtao <sheseerzuo123>
2020-06-24 15:02:17 +0800

    Issue 前端允许跨域

--- flash-vue-admin/src/utils/request.js
+++ flash-vue-admin/src/utils/request.js
@@ -7,7 +7,7 @@ import router from '@/router'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  withCredentials: true, // send cookies when cross-domain requests
+  withCredentials: false, // send cookies when cross-domain requests
   timeout: 25000 // request timeout
 })
 
