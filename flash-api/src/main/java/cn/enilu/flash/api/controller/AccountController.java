56e0a3b6c1a5442e56c365201e7d40683cf4f49e
enilu <sheseerzuo123>
2020-07-13 17:39:39 +0800

    Fix #69 shiro导致缓存注解失效

--- flash-api/src/main/java/cn/enilu/flash/api/controller/AccountController.java
+++ flash-api/src/main/java/cn/enilu/flash/api/controller/AccountController.java
@@ -16,6 +16,7 @@ import org.nutz.mapl.Mapl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,6 +38,7 @@ public class AccountController extends BaseController{
      private Logger logger = LoggerFactory.getLogger(AccountController.class);
 
     @Autowired
+    @Lazy
     private UserService userService;
 
     /**
