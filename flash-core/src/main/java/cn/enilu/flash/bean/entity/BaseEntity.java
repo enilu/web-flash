fb56cfa97e4a5a66e5cc0cec82d201936b25ec00
enilu <zhangtao54@gmail.com>
2020-07-20 22:51:26 +0800

    Change BaseEntity 创建日期和更新日期使用hibernate注解代替spring的注解

--- flash-core/src/main/java/cn/enilu/flash/bean/entity/BaseEntity.java
+++ flash-core/src/main/java/cn/enilu/flash/bean/entity/BaseEntity.java
@@ -2,10 +2,10 @@ package cn.enilu.flash.bean.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
@@ -29,13 +29,13 @@ public abstract class BaseEntity implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
-    @CreatedDate
+    @CreationTimestamp
     @Column(name = "create_time",columnDefinition="DATETIME COMMENT '创建时间/注册时间'",updatable = false)
     private Date createTime;
     @Column(name = "create_by",columnDefinition="bigint COMMENT '创建人'",updatable = false)
     @CreatedBy
     private Long createBy;
-    @LastModifiedDate
+    @UpdateTimestamp
     @Column(name = "modify_time",columnDefinition="DATETIME COMMENT '最后更新时间'")
     private Date modifyTime;
     @LastModifiedBy
