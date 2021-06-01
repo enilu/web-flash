bbca94254106d7e629a08176572d5ff5d407aa30
enilu <82552623@qq.com>
2021-06-01 22:32:41 +0800

    Issue 为数据源添加事务管理器

--- flash-api/src/main/java/cn/enilu/flash/api/ApiApplication.java
+++ flash-api/src/main/java/cn/enilu/flash/api/ApiApplication.java
@@ -7,9 +7,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * ApiApplication
@@ -28,6 +33,12 @@ public class ApiApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ApiApplication.class);
+    }
+
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 
     public static void main(String[] args) {
