063e39d1790066734fe82c1209718a942016d141
enilu <82552623@qq.com>
2021-07-25 16:56:52 +0800

    Add Actovoti listener

---
+++ flash-workflow/src/main/java/cn/enilu/flash/workflow/WorkFlowRequestEndListener.java
@@ -0,0 +1,15 @@
+package cn.enilu.flash.workflow;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+
+/**
+ * @author ：enilu
+ * @date ：Created in 2021/7/25 12:24
+ */
+public class WorkFlowRequestEndListener implements ExecutionListener {
+    @Override
+    public void notify(DelegateExecution delegateExecution) {
+
+    }
+}
